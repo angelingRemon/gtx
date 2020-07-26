71a01174b348a2498384a6691acdfda3352c3b3e
CcXiaoYu <XiaoYu_7777777@163.com>
2020-07-27 02:01:31 +0800

    '第一天工作'

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,35 @@ const app = getApp()
 
 Page({
   data: {
-  
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      url: '../../assets/images/banner.jpg'
+    }, {
+      id: 1,
+      url: '../../assets/images/banner.jpg',
+      }, {
+        id: 2,
+        url: '../../assets/images/banner.jpg',
+      }],
   },
 
-  onLoad: function () {
-  
+  onLoad: function() {
+    this.towerSwiper('swiperList');
+  },
+  towerSwiper(name) {
+    let list = this.data[name];
+    for (let i = 0; i < list.length; i++) {
+      list[i].zIndex = parseInt(list.length / 2) + 1 - Math.abs(i - parseInt(list.length / 2))
+      list[i].mLeft = i - parseInt(list.length / 2)
+    }
+    this.setData({
+      swiperList: list
+    })
+  },
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
   }
-  
 })
